05535bda495818afd33856f26fba94ba4c62f2a8
hwookim <gusdn9754@gmail.com>
2020-07-06 17:45:09 +0900

    feat: Member Entity 구현
    트랜젝션 내에서는 캐시된 값을 가져오기 때문에 같은 주소값의 객체를 사용하게 된다.
    
    SpringFramework의 힘을 빌리지 않는다면 코드가 매우 복잡해진다.

---
+++ src/main/java/tacademy/jpa/example/domain/Member.java
@@ -0,0 +1,23 @@
+package tacademy.jpa.example.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    protected Member() {
+    }
+
+}
---
+++ src/test/java/tacademy/jpa/example/domain/MemberTest.java
@@ -0,0 +1,58 @@
+package tacademy.jpa.example.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class MemberTest {
+
+    @Autowired
+    MemberRepository members;
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    void saveWithoutSpringFramework() {
+        // 사실 Spring 위에서 돌아가지만... 그래도 최대한 비슷한 코드로
+
+        // EntityManagerFactory emf = Persistence.createEntityManagerFactory("~~");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member expected = new Member();
+            expected.setId(100L);
+            expected.setName("test");
+
+            em.persist(expected);
+            tx.commit();
+
+            Member actual = em.find(Member.class, 100L);
+            assertThat(actual).isEqualTo(expected);
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    @Test
+    void save() {
+        Member expected = new Member();
+        expected.setName("test");
+
+        Member actual = members.save(expected);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
