0d5cf14cd350b2558b08b9ecd57d405e6a449d12
hwookim <gusdn9754@gmail.com>
2020-07-06 16:28:26 +0900

    Initial commit

---
+++ src/test/java/tacademy/jpa/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package tacademy.jpa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
